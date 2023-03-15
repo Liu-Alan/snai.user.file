9d142ad45aa4c5161a57189c71384160e41e0e5a
Liu-Alan <xiaolin-liu@qq.com>
2023-03-15 23:43:51 +0800

    login

--- main.go
+++ main.go
@@ -50,7 +50,6 @@ func RegUser(c *gin.Context) {
 				sc := s.Text()
 				lastIndex := strings.LastIndex(sc, "|")
 				uaccount := sc[:lastIndex]
-				fmt.Println(uaccount)
 				if account == uaccount {
 					message = "Account already exists"
 					status = 400
@@ -88,9 +87,60 @@ outJson:
 	})
 }
 
+func Login(c *gin.Context) {
+	account := strings.TrimSpace(c.Query("account"))
+	password := strings.TrimSpace(c.Query("password"))
+
+	message := ""
+	status := 200
+
+	if account == "" || password == "" {
+		message = "Account or password cannot be empty"
+		status = 400
+	} else {
+		f, err := os.Open("user.log")
+		defer f.Close()
+		if err != nil {
+			message = "Service error" //err.Error()
+			status = 400
+		} else {
+			s := bufio.NewScanner(f)
+			for s.Scan() {
+				sc := s.Text()
+				lastIndex := strings.LastIndex(sc, "|")
+				uaccount := sc[:lastIndex]
+				if account == uaccount {
+					h := md5.New()
+					io.WriteString(h, password)
+					pwdmd5 := hex.EncodeToString(h.Sum(nil))
+					upassword := sc[lastIndex+1:]
+					if pwdmd5 == upassword {
+						message = "Welcome " + account
+						status = 200
+						goto outJson
+					} else {
+						message = "Account or password is wrong"
+						status = 400
+						goto outJson
+					}
+				}
+			}
+			message = "Account or password is wrong"
+			status = 400
+		}
+
+	}
+outJson:
+	c.JSON(200, gin.H{
+		"status":  status,
+		"message": message,
+	})
+}
+
 func main() {
 	router := gin.Default()
 	router.Use(MiddlewaresCors())
 	router.GET("/reguser", RegUser)
+	router.GET("/login", Login)
 	router.Run(":8080")
 }
